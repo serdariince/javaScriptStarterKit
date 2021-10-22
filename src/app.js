a4a2f448a66ceb1d34059f5f4cfa7eb1e4aaa954
Serdar İnce <serdariince@gmail.com>
2021-10-22 21:29:10 +0300

    imzala gitsin

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ console.log("Merhaba Serdar İNCE")
 let konutKredisi=["serdar","Hasan"];
 console.log("<ul>")
 for (let i = 0; i < konutKredisi.length; i++) {
-    
-    console.log(konutKredisi[i]);
+    console.log("<li>"+konutKredisi[i]+"</li>");
 } 
 console.log("</ul>")
